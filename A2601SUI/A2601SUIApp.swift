ea92f352a6841469fc515a8ea4e25ba61d8d428f
Danny Shen <shenfive@gmail.com>
2026-01-22 11:12:58 +0800

    Initial Commit

---
+++ A2601SUI/A2601SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  A2601SUIApp.swift
+//  A2601SUI
+//
+//  Created by Danny Shen on 2026/1/22.
+//
+
+import SwiftUI
+
+@main
+struct A2601SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A2601SUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A2601SUI
+//
+//  Created by Danny Shen on 2026/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
