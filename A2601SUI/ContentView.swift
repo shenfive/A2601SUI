c5bbd4c3d46c6fb8db58f0afc191a08f9f97a54a
Danny Shen <shenfive@gmail.com>
2026-01-22 11:55:13 +0800

    調整後的簡介

--- A2601SUI/ContentView.swift
+++ A2601SUI/ContentView.swift
@@ -10,19 +10,22 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack(alignment: .center) {
-      
-            Image(systemName: "person.circle.fill")       // 使用 System Image
-                .foregroundColor(.blue)
-                .shadow(radius: 5)
             Spacer()
+            Image(systemName: "person.circle.fill")       // 使用 System Image
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200)
+                .foregroundColor(.blue)
+            
             Text("潤五 Danny") // 名字文字
                 .font(.title)
-            Spacer()
             Text("Apple 開發者者，擅長 SwiftUI、Xcode 也會一點  Android 和其他程式")   // 簡介文字
                 .font(.subheadline)
                 .foregroundColor(.gray)
                 .multilineTextAlignment(.center)
                 .padding(.horizontal)
+            Spacer()
+            Spacer()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(.systemGroupedBackground))
